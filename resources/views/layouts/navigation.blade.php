7a2347a580355e201c376e139e31434618403733
Miguel Figueroa <miguelfigueroa90@gmail.com>
2023-08-16 22:03:56 -0400

    Updating menu elements

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -25,7 +25,7 @@
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('ingredient.orders')" :active="request()->routeIs('ingredient.orders')">
-                        {{ __('Orders') }}
+                        {{ __('Pending Orders') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -81,6 +81,18 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('ingredient.index')" :active="request()->routeIs('ingredient.index')">
+                {{ __('Ingredients') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('ingredient.orders')" :active="request()->routeIs('ingredient.orders')">
+                {{ __('Pending Orders') }}
             </x-responsive-nav-link>
         </div>
 
